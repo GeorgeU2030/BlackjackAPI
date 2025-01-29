210e84df43206162a36fb536d026d5fc31d75062
GeorgeU2030 <georgeu2021@gmail.com>
2025-01-29 14:00:31 -0500

    feat: add cors

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -20,6 +20,16 @@ public func configure(_ app: Application) async throws {
     // run migrations
     try await app.autoMigrate()
     
+    // cors configuration
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .all,
+        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin]
+    )
+    let cors = CORSMiddleware(configuration: corsConfiguration)
+    
+    app.middleware.use(cors, at: .beginning)
+    
     // register routes
     try routes(app)
 }
